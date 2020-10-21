36e0d5e866b50f6fa7fa079a4335abab9bd78dda
Артем Крылов <kav128@outlook.com>
2020-10-21 20:35:19 +0300

    add proxy rotation

--- src/main/java/com/wine/to/up/winestyle/parser/service/service/implementation/document/ScrapingService.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/service/implementation/document/ScrapingService.java
@@ -1,5 +1,6 @@
 package com.wine.to.up.winestyle.parser.service.service.implementation.document;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,14 +9,35 @@ import org.springframework.stereotype.Service;
 import javax.net.ssl.SSLException;
 import java.io.IOException;
 import java.net.ConnectException;
+import java.net.Proxy;
 import java.net.SocketTimeoutException;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Класс, который парсит страницы сайта. Скачивает html и возвращает Document.
  */
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ScrapingService {
+
+    private static final ProxyService proxyService = new ProxyService();
+
+    private static final List<Proxy> proxies;
+    private static Iterator<Proxy> it;
+
+    static {
+         proxies = proxyService.getProxies();
+    }
+
+    public synchronized Proxy getNextProxyRoundRobin() {
+        if (it == null || !it.hasNext()) {
+            it = proxies.iterator();
+        }
+        return it.next();
+    }
+
     /**
      * Достаем док из ссылки
      * @param url Ссылка на страницу
@@ -32,6 +54,7 @@ public class ScrapingService {
                                 "AppleWebKit/537.36 (KHTML, like Gecko) " +
                                 "Chrome/85.0.4183.121 " +
                                 "Safari/537.36")
+                        .proxy(getNextProxyRoundRobin())
                         .get();
             } catch (SocketTimeoutException | SSLException | ConnectException e) {
                 log.error("Couldn't get a connection to website!", e);
@@ -40,7 +63,6 @@ public class ScrapingService {
                 log.error("An error occurs whilst fetching the URL!", e);
             }
         }
-        Thread.sleep(625);
         return doc;
     }
 }
