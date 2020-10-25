5cd5903a4714e896536542f900687c86c5ac0375
Nikita <baby0benz@gmail.com>
2020-10-25 14:56:34 +0300

    remove redundant annotations simplify ComponentScan annotation

--- src/main/java/com/wine/to/up/winestyle/parser/service/ServiceApplication.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/ServiceApplication.java
@@ -2,16 +2,12 @@ package com.wine.to.up.winestyle.parser.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan({"com.wine.to.up", "com.wine.to.up.winestyle.parser.service.service"})
-@EntityScan("com.wine.to.up.winestyle.parser.service.domain")
-@EnableJpaRepositories("com.wine.to.up.winestyle.parser.service.repository")
+@ComponentScan("com.wine.to.up")
 @EnableSwagger2
 @EnableScheduling
 public class ServiceApplication {
