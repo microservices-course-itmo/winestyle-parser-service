16336fec918bdd9adecdeea4a923c3aa70021703
Nikita <baby0benz@gmail.com>
2020-11-10 02:00:45 +0300

    add scheduled proxy checking

--- src/main/java/com/wine/to/up/winestyle/parser/service/service/implementation/scheduling/AlcoholParsingScheduler.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/service/implementation/scheduling/AlcoholParsingScheduler.java
@@ -2,16 +2,20 @@ package com.wine.to.up.winestyle.parser.service.service.implementation.schedulin
 
 import com.wine.to.up.winestyle.parser.service.controller.exception.ServiceIsBusyException;
 import com.wine.to.up.winestyle.parser.service.controller.exception.UnsupportedAlcoholTypeException;
+import com.wine.to.up.winestyle.parser.service.service.implementation.controller.MainControllerService;
 import com.wine.to.up.winestyle.parser.service.service.implementation.controller.ParsingControllerService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AlcoholParsingScheduler {
     private final ParsingControllerService parsingControllerService;
+    private final MainControllerService mainControllerService;
 
     @SneakyThrows(UnsupportedAlcoholTypeException.class)
     @Scheduled(cron = "${scheduler.cron.expression}")
@@ -32,6 +36,15 @@ public class AlcoholParsingScheduler {
         } catch (ServiceIsBusyException e) {
             Thread.sleep(3600000);
             onScheduleParseSparkling();
+        }
+    }
+
+    @Scheduled(fixedRate = 3600000)
+    public void onScheduleLoadProxies() {
+        try {
+            mainControllerService.startProxyInitJob(20000);
+        } catch (ServiceIsBusyException e) {
+            log.info("Scheduled proxy initialization job is rejected. {}", e.getMessage());
         }
     }
 }
