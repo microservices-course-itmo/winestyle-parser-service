85dca11d1fadf01788852b59b3191b1d3fadd061
Nshmendyuk <Nshmendyuk.ru@yandex.ru>
2020-11-22 04:36:10 +0300

    added a test to check getting url by alcohol type

--- src/test/java/com/wine/to/up/winestyle/parser/service/service/implementation/controller/ParsingControllerServiceTest.java
+++ src/test/java/com/wine/to/up/winestyle/parser/service/service/implementation/controller/ParsingControllerServiceTest.java
@@ -1,16 +1,24 @@
 package com.wine.to.up.winestyle.parser.service.service.implementation.controller;
 
+import com.google.common.collect.ImmutableMap;
 import com.wine.to.up.winestyle.parser.service.service.implementation.helpers.StatusService;
+import com.wine.to.up.winestyle.parser.service.service.implementation.helpers.enums.AlcoholType;
 import com.wine.to.up.winestyle.parser.service.service.implementation.helpers.enums.ServiceType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParsingControllerServiceTest {
 
     private StatusService statusService;
+
+    private ImmutableMap<AlcoholType, String> SUPPORTED_ALCOHOL_URLS = ImmutableMap .<AlcoholType, String>builder()
+                .put(AlcoholType.WINE, "WINE_URL")
+                .put(AlcoholType.SPARKLING, "SPARKLING_URL")
+                .build();
 
     @BeforeEach
     public void setUp(){
@@ -32,5 +40,11 @@ class ParsingControllerServiceTest {
         assertFalse(statusService.tryBusy(ServiceType.PROXY));
         statusService.release(ServiceType.PROXY);
         assertTrue(statusService.tryBusy(ServiceType.PROXY));
+    }
+
+    @Test
+    void getAlcoholTypeUrl() {
+        assertEquals("WINE_URL", SUPPORTED_ALCOHOL_URLS.get(AlcoholType.WINE));
+        assertEquals("SPARKLING_URL", SUPPORTED_ALCOHOL_URLS.get(AlcoholType.SPARKLING));
     }
 }
