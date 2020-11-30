4a6fe58f7587489704c74d303149f9526aac2ea2
Nikita <baby0benz@gmail.com>
2020-11-30 16:05:55 +0300

    change proxy services scope avoiding field null values

--- src/main/java/com/wine/to/up/winestyle/parser/service/service/implementation/document/ProxyWebPageLoader.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/service/implementation/document/ProxyWebPageLoader.java
@@ -1,13 +1,20 @@
 package com.wine.to.up.winestyle.parser.service.service.implementation.document;
 
 import com.wine.to.up.winestyle.parser.service.service.UnstableLoader;
+import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.Proxy;
 
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@RequiredArgsConstructor
 public class ProxyWebPageLoader implements UnstableLoader {
     private final Proxy proxy;
     private int failuresCount;
