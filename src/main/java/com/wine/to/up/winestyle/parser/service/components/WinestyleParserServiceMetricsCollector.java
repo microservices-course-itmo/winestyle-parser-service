1e79617a8fc12b433a4527fe6733d8621d2025da
Nikita <baby0benz@gmail.com>
2020-12-01 02:18:46 +0300

    add new metrics to collector

--- src/main/java/com/wine/to/up/winestyle/parser/service/components/WinestyleParserServiceMetricsCollector.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/components/WinestyleParserServiceMetricsCollector.java
@@ -1,6 +1,7 @@
 package com.wine.to.up.winestyle.parser.service.components;
 
 import com.wine.to.up.commonlib.metrics.CommonMetricsCollector;
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,25 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class WinestyleParserServiceMetricsCollector extends CommonMetricsCollector {
+    private static final String PARSING_STARTED_COUNTER = "parsing_started_total";
+    private static final String PARSING_COMPLETE_COUNTER = "parsing_complete_total";
+    private static final String PUBLISHED_COUNTER = "wines_published_to_kafka_count";
+
+    public  static final String PARSER_NAME_TAG = "winestyle";
+
     public WinestyleParserServiceMetricsCollector(@Value("${spring.kafka.metrics.service-name}") String serviceName) {
         super(serviceName);
+    }
+
+    public void incParsingStarted(String parserName) {
+        Metrics.counter(PARSING_STARTED_COUNTER, PARSER_NAME_TAG, parserName).increment();
+    }
+
+    public void incParsingComplete(String parserName) {
+        Metrics.counter(PARSING_COMPLETE_COUNTER, PARSER_NAME_TAG, parserName).increment();
+    }
+
+    public void incPublished(String parserName) {
+        Metrics.counter(PUBLISHED_COUNTER, PARSER_NAME_TAG, parserName).increment();
     }
 }
