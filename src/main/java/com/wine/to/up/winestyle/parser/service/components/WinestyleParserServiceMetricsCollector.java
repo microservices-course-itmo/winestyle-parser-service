19d1fa2d93c297aea828f33ec953ddb8a603d77b
Nikita <baby0benz@gmail.com>
2020-12-01 14:56:57 +0300

    add gauge and summary metrics to WinestyleParserServiceMetricsCollector

--- src/main/java/com/wine/to/up/winestyle/parser/service/components/WinestyleParserServiceMetricsCollector.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/components/WinestyleParserServiceMetricsCollector.java
@@ -17,6 +17,15 @@ public final class WinestyleParserServiceMetricsCollector extends CommonMetricsC
     private static final String PARSING_COMPLETE_COUNTER = "parsing_complete_total";
     private static final String PUBLISHED_COUNTER = "wines_published_to_kafka_count";
 
+    private static final String IN_PROGRESS_GAUGE = "parsing_in_progress";
+    private static final String TIME_SINCE_LAST_SUCCEED_GAUGE = "time_since_last_succeeded_parsing";
+
+    private static final String PARSING_DURATION_DURATION_SUMMARY = "parsing_process_duration";
+    private static final String DETAILS_FETCHING_DURATION_SUMMARY = "wine_details_fetching_duration";
+    private static final String PAGE_FETCHING_DURATION_SUMMARY = "wine_page_fetching_duration";
+    private static final String PAGE_PARSING_DURATION_SUMMARY = "wine_page_parsing_duration";
+    private static final String DETAILS_PARSING_DURATION_SUMMARY = "wine_details_parsing_duration";
+
     public static final String PARSER_NAME_TAG = "winestyle";
 
     private WinestyleParserServiceMetricsCollector(@Value("${spring.kafka.metrics.service-name}") String serviceName) {
@@ -29,6 +38,10 @@ public final class WinestyleParserServiceMetricsCollector extends CommonMetricsC
 
     public static void incParsingComplete(String parserName) {
         Metrics.counter(PARSING_COMPLETE_COUNTER, PARSER_NAME_TAG, parserName).increment();
+    }
+
+    public static void incPublished(String parserName) {
+        Metrics.counter(PUBLISHED_COUNTER, PARSER_NAME_TAG, parserName).increment();
     }
 
     public static void incPublished(String parserName) {
