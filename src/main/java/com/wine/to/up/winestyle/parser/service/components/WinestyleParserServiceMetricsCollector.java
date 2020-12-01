2b596f5e9512e1efb54974637a13db2a96c5fa39
Nikita <baby0benz@gmail.com>
2020-12-01 11:50:04 +0300

    make WinestyleParserServiceMetricsCollector final with static methods

--- src/main/java/com/wine/to/up/winestyle/parser/service/components/WinestyleParserServiceMetricsCollector.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/components/WinestyleParserServiceMetricsCollector.java
@@ -12,26 +12,26 @@ import org.springframework.stereotype.Component;
  * Prometheus' metrics exposed at /metrics-prometheus
  */
 @Component
-public class WinestyleParserServiceMetricsCollector extends CommonMetricsCollector {
+public final class WinestyleParserServiceMetricsCollector extends CommonMetricsCollector {
     private static final String PARSING_STARTED_COUNTER = "parsing_started_total";
     private static final String PARSING_COMPLETE_COUNTER = "parsing_complete_total";
     private static final String PUBLISHED_COUNTER = "wines_published_to_kafka_count";
 
-    public  static final String PARSER_NAME_TAG = "winestyle";
+    public static final String PARSER_NAME_TAG = "winestyle";
 
-    public WinestyleParserServiceMetricsCollector(@Value("${spring.kafka.metrics.service-name}") String serviceName) {
+    private WinestyleParserServiceMetricsCollector(@Value("${spring.kafka.metrics.service-name}") String serviceName) {
         super(serviceName);
     }
 
-    public void incParsingStarted(String parserName) {
+    public static void incParsingStarted(String parserName) {
         Metrics.counter(PARSING_STARTED_COUNTER, PARSER_NAME_TAG, parserName).increment();
     }
 
-    public void incParsingComplete(String parserName) {
+    public static void incParsingComplete(String parserName) {
         Metrics.counter(PARSING_COMPLETE_COUNTER, PARSER_NAME_TAG, parserName).increment();
     }
 
-    public void incPublished(String parserName) {
+    public static void incPublished(String parserName) {
         Metrics.counter(PUBLISHED_COUNTER, PARSER_NAME_TAG, parserName).increment();
     }
 }
