6beaa3bfd044e94e1d6bac89dfe08e9b57c87d2b
Nshmendyuk <Nshmendyuk.ru@yandex.ru>
2020-12-02 19:02:16 +0300

    added a test to check spring runner

---
+++ src/test/java/com/wine/to/up/winestyle/parser/service/ServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.wine.to.up.winestyle.parser.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest()
+class ServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
