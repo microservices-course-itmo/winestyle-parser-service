2571c3bd9062c9b6e704710413a019b161d86427
Nshmendyuk <Nshmendyuk.ru@yandex.ru>
2020-12-02 18:51:39 +0300

    updated tests for ErrorOnSaving

--- src/test/java/com/wine/to/up/winestyle/parser/service/domain/entity/ErrorOnSavingTest.java
+++ src/test/java/com/wine/to/up/winestyle/parser/service/domain/entity/ErrorOnSavingTest.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ErrorOnSavingTest {
     static Alcohol alcohol;
-    static Alcohol alcoholNull;
     static Timestamp timestamp;
     static String error;
     String expectedErrorOnSavingString = "ErrorOnSaving(id=1, " +
@@ -23,7 +22,14 @@ class ErrorOnSavingTest {
             "manufacturer=null, brand=null, color=null, country=null, region=null, " +
             "volume=null, strength=null, sugar=null, price=null, grape=null, " +
             "taste=null, aroma=null, foodPairing=null, description=null, rating=null, " +
-            "unsavedId=null, timestamp=1970-01-01 03:00:00.0, error=test error string)";
+            "unsavedId=null, timestamp=null, error=null)";
+
+    String expectedErrorOnSavingStringOf = "ErrorOnSaving(id=null, " +
+            "type=test, name=test, url=test, imageUrl=test, cropYear=1990, " +
+            "manufacturer=test, brand=test, color=test, country=test, region=test, " +
+            "volume=1.0, strength=1.0, sugar=test, price=1.0, grape=test, " +
+            "taste=test, aroma=test, foodPairing=test, description=test, rating=1.0, " +
+            "unsavedId=1, timestamp=1970-01-01 03:00:00.0, error=test error string)";
 
     @BeforeAll
     static void setUp() {
@@ -36,68 +42,31 @@ class ErrorOnSavingTest {
                 .grape("test").taste("test").aroma("test").foodPairing("test")
                 .description("test").rating(1F)
                 .build();
-        alcoholNull = new Alcohol();
     }
 
     @Test
     void of() {
-        ErrorOnSaving errorOnSaving = ErrorOnSaving.builder()
-                .id(alcohol.getId())
-                .name(alcohol.getName())
-                .type(alcohol.getType())
-                .url(alcohol.getUrl())
-                .imageUrl(alcohol.getImageUrl())
-                .cropYear(alcohol.getCropYear() == null ? null : alcohol.getCropYear().toString())
-                .manufacturer(alcohol.getManufacturer())
-                .brand(alcohol.getBrand())
-                .color(alcohol.getColor())
-                .country(alcohol.getCountry())
-                .region(alcohol.getRegion())
-                .volume(alcohol.getVolume() == null ? null : alcohol.getVolume().toString())
-                .strength(alcohol.getStrength() == null ? null : alcohol.getStrength().toString())
-                .sugar(alcohol.getSugar())
-                .price(alcohol.getPrice() == null ? null : alcohol.getPrice().toString())
-                .grape(alcohol.getGrape())
-                .taste(alcohol.getTaste())
-                .aroma(alcohol.getAroma())
-                .foodPairing(alcohol.getFoodPairing())
-                .description(alcohol.getDescription())
-                .rating(alcohol.getRating() == null ? null : alcohol.getRating().toString())
-                .unsavedId(alcohol.getId())
-                .error(error)
-                .timestamp(timestamp)
-                .build();
-        assertEquals(expectedErrorOnSavingString, errorOnSaving.toString());
+        ErrorOnSaving errorOnSaving = ErrorOnSaving.of(alcohol, timestamp, error);
+        String toStringValues = errorOnSaving.toString();
+        assertEquals(expectedErrorOnSavingStringOf, toStringValues);
     }
 
     @Test
-    void ofNull(){
-        ErrorOnSaving errorOnSaving = ErrorOnSaving.builder()
-                .id(alcoholNull.getId())
-                .name(alcoholNull.getName())
-                .type(alcoholNull.getType())
-                .url(alcoholNull.getUrl())
-                .imageUrl(alcoholNull.getImageUrl())
-                .cropYear(alcoholNull.getCropYear() == null ? null : alcoholNull.getCropYear().toString())
-                .manufacturer(alcoholNull.getManufacturer())
-                .brand(alcoholNull.getBrand())
-                .color(alcoholNull.getColor())
-                .country(alcoholNull.getCountry())
-                .region(alcoholNull.getRegion())
-                .volume(alcoholNull.getVolume() == null ? null : alcoholNull.getVolume().toString())
-                .strength(alcoholNull.getStrength() == null ? null : alcohol.getStrength().toString())
-                .sugar(alcoholNull.getSugar())
-                .price(alcoholNull.getPrice() == null ? null : alcoholNull.getPrice().toString())
-                .grape(alcoholNull.getGrape())
-                .taste(alcoholNull.getTaste())
-                .aroma(alcoholNull.getAroma())
-                .foodPairing(alcoholNull.getFoodPairing())
-                .description(alcoholNull.getDescription())
-                .rating(alcoholNull.getRating() == null ? null : alcoholNull.getRating().toString())
-                .unsavedId(alcoholNull.getId())
-                .error(error)
-                .timestamp(timestamp)
-                .build();
-        assertEquals(expectedErrorOnSavingNullString, errorOnSaving.toString());
+    void construct() {
+        ErrorOnSaving errorOnSavingNull = new ErrorOnSaving(1L, "test", "test",
+                "test", "test", "1990", "test",
+                "test", "test", "test", "test",
+                "1.0", 1F, "test", "1.0", "test",
+                "test", "test", "test",
+                "test", "1.0", 1L, timestamp, "test error string");
+        String stringValues = errorOnSavingNull.toString();
+        assertEquals(expectedErrorOnSavingString, stringValues);
+    }
+
+    @Test
+    void constructNull() {
+        ErrorOnSaving errorOnSavingNull = new ErrorOnSaving();
+        String stringNullValues = errorOnSavingNull.toString();
+        assertEquals(expectedErrorOnSavingNullString, stringNullValues);
     }
 }
