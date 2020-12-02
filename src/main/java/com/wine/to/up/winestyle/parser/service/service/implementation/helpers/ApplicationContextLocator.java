ff85073e2389c4df3db766dd6d0dd74b9bf0dc01
Nikita <baby0benz@gmail.com>
2020-12-02 19:43:26 +0300

    add static ContextInitializer

--- src/main/java/com/wine/to/up/winestyle/parser/service/service/implementation/helpers/ApplicationContextLocator.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/service/implementation/helpers/ApplicationContextLocator.java
@@ -1,6 +1,5 @@
 package com.wine.to.up.winestyle.parser.service.service.implementation.helpers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +11,11 @@ public class ApplicationContextLocator {
         return applicationContext;
     }
 
-    public ApplicationContextLocator() {
+    private ApplicationContextLocator() {
         super();
     }
 
-    @Autowired
-    public void setApplicationContext(ApplicationContext applicationContext) {
+    public static void setApplicationContext(ApplicationContext applicationContext) {
         ApplicationContextLocator.applicationContext = applicationContext;
     }
 }
