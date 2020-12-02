ea629b9de95ec3c5db2e3fdd061c76194156da1e
Nshmendyuk <Nshmendyuk.ru@yandex.ru>
2020-12-02 18:51:14 +0300

    updated tests for apiError

--- src/test/java/com/wine/to/up/winestyle/parser/service/domain/ApiErrorTest.java
+++ src/test/java/com/wine/to/up/winestyle/parser/service/domain/ApiErrorTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ApiErrorTest {
 
     static ApiError apiError;
-    static String expectedString = "[test fail]";
+    static String expectedString = "[test, fail]";
 
     @BeforeAll
     static void setUp() {
@@ -24,5 +24,23 @@ class ApiErrorTest {
     void getErrors() {
         String s = apiError.getErrors().toString();
         assertEquals(expectedString, s);
+    }
+
+    @Test
+    void setErrors() {
+        List<String> errors = new ArrayList<>();
+        errors.add("test");
+        errors.add("fail");
+        apiError.setErrors(errors);
+        assertEquals(errors, apiError.getErrors());
+    }
+
+    @Test
+    void constructApiErrors() {
+        List<String> errors = new ArrayList<>();
+        errors.add("test");
+        errors.add("fail");
+        ApiError apiError = new ApiError(errors);
+        assertEquals(errors, apiError.getErrors());
     }
 }
