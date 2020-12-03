fbac0afca66afe5598112f29f033a6ca741fc4cf
Nikita <baby0benz@gmail.com>
2020-12-04 01:07:48 +0300

    add new scheduler class for checking for different app states completely rewrite all summary logic and change contracts of other methods

--- src/main/java/com/wine/to/up/winestyle/parser/service/components/WinestyleParserServiceMetricsCollector.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/components/WinestyleParserServiceMetricsCollector.java
@@ -6,6 +6,8 @@ import io.micrometer.core.instrument.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -23,55 +25,63 @@ public final class WinestyleParserServiceMetricsCollector extends CommonMetricsC
     private static final String IN_PROGRESS_GAUGE = "parsing_in_progress";
     private static final String TIME_SINCE_LAST_SUCCEED_GAUGE = "time_since_last_succeeded_parsing";
 
-    private static final String PARSING_PROCESS_DURATION_SUMMARY = "parsing_process_duration";
     private static final String DETAILS_FETCHING_DURATION_SUMMARY = "wine_details_fetching_duration";
     private static final String PAGE_FETCHING_DURATION_SUMMARY = "wine_page_fetching_duration";
     private static final String PAGE_PARSING_DURATION_SUMMARY = "wine_page_parsing_duration";
     private static final String DETAILS_PARSING_DURATION_SUMMARY = "wine_details_parsing_duration";
 
+    private static final String PARSER_NAME = "winestyle_parser_service";
     public static final String PARSER_NAME_TAG = "winestyle";
 
     public WinestyleParserServiceMetricsCollector(@Value("${spring.kafka.metrics.service-name}") String serviceName) {
         super(serviceName);
     }
 
-    public static void incParsingStarted(String parserName) {
-        Metrics.counter(PARSING_STARTED_COUNTER, PARSER_NAME_TAG, parserName).increment();
+    public static void incParsingStarted() {
+        Metrics.counter(PARSING_STARTED_COUNTER, PARSER_NAME_TAG, PARSER_NAME).increment();
     }
 
-    public static void incParsingComplete(String parserName) {
-        Metrics.counter(PARSING_COMPLETE_COUNTER, PARSER_NAME_TAG, parserName).increment();
+    public static void incParsingComplete() {
+        Metrics.counter(PARSING_COMPLETE_COUNTER, PARSER_NAME_TAG, PARSER_NAME).increment();
     }
 
-    public static void incPublished(String parserName) {
-        Metrics.counter(PUBLISHED_COUNTER, PARSER_NAME_TAG, parserName).increment();
+    public static void incPublished() {
+        Metrics.counter(PUBLISHED_COUNTER, PARSER_NAME_TAG, PARSER_NAME).increment();
     }
 
-    public static void gaugeInProgress(String parserName, Integer currentValue) {
-        Metrics.gauge(IN_PROGRESS_GAUGE, List.of(Tag.of(PARSER_NAME_TAG, parserName)), currentValue);
+    public static void gaugeInProgress(int currentValue) {
+        Metrics.gauge(IN_PROGRESS_GAUGE, List.of(Tag.of(PARSER_NAME_TAG, PARSER_NAME)), currentValue);
     }
 
-    public static void gaugeSinceLastSucceed(String parserName, Long currentValue) {
-        Metrics.gauge(TIME_SINCE_LAST_SUCCEED_GAUGE, List.of(Tag.of(PARSER_NAME_TAG, parserName)), currentValue);
+    public static void gaugeSinceLastSucceed(double currentValue) {
+        Metrics.gauge(TIME_SINCE_LAST_SUCCEED_GAUGE, List.of(Tag.of(PARSER_NAME_TAG, PARSER_NAME)), currentValue);
     }
 
-    public static void sumProcessParsing(String parserName) {
-        Metrics.summary(PARSING_PROCESS_DURATION_SUMMARY, List.of(Tag.of(PARSER_NAME_TAG, parserName)));
+    public static void sumPageFetchingDuration(LocalDateTime timeBeforeFetching, LocalDateTime timeAfterFetching) {
+        Metrics.summary(
+                PAGE_FETCHING_DURATION_SUMMARY,
+                List.of(Tag.of(PARSER_NAME_TAG, PARSER_NAME))
+        ).record(Duration.between(timeBeforeFetching, timeAfterFetching).toNanos() / 1e9d);
     }
 
-    public static void sumDetailsFetching(String parserName) {
-        Metrics.summary(DETAILS_FETCHING_DURATION_SUMMARY, List.of(Tag.of(PARSER_NAME_TAG, parserName)));
+    public static void sumDetailsFetchingDuration(LocalDateTime timeBeforeFetching, LocalDateTime timeAfterFetching) {
+        Metrics.summary(
+                DETAILS_FETCHING_DURATION_SUMMARY,
+                List.of(Tag.of(PARSER_NAME_TAG, PARSER_NAME))
+        ).record(Duration.between(timeBeforeFetching, timeAfterFetching).toNanos() / 1e9d);
     }
 
-    public static void sumPageFetching(String parserName) {
-        Metrics.summary(PAGE_FETCHING_DURATION_SUMMARY, List.of(Tag.of(PARSER_NAME_TAG, parserName)));
+    public static void sumPageParsingDuration(LocalDateTime timeBeforeParsing, LocalDateTime timeAfterParsing) {
+        Metrics.summary(
+                PAGE_PARSING_DURATION_SUMMARY,
+                List.of(Tag.of(PARSER_NAME_TAG, PARSER_NAME))
+        ).record(Duration.between(timeBeforeParsing, timeAfterParsing).toNanos() / 1e9d);
     }
 
-    public static void sumPageParsing(String parserName) {
-        Metrics.summary(PAGE_PARSING_DURATION_SUMMARY, List.of(Tag.of(PARSER_NAME_TAG, parserName)));
-    }
-
-    public static void sumDetailsParsing(String parserName) {
-        Metrics.summary(DETAILS_PARSING_DURATION_SUMMARY, List.of(Tag.of(PARSER_NAME_TAG, parserName)));
+    public static void sumDetailsParsingDuration(LocalDateTime timeBeforeParsing, LocalDateTime timeAfterParsing) {
+        Metrics.summary(
+                DETAILS_PARSING_DURATION_SUMMARY,
+                List.of(Tag.of(PARSER_NAME_TAG, PARSER_NAME))
+        ).record(Duration.between(timeBeforeParsing, timeAfterParsing).toNanos() / 1e9d);
     }
 }
