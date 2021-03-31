6eb467c4dbdccddb6f62b0c7a444b62731d93c06
Артем Крылов <kav128@outlook.com>
2021-03-31 18:57:03 +0300

    fix "Catalog's pages parsed" metric

--- src/main/java/com/wine/to/up/winestyle/parser/service/logging/NotableEvents.java
+++ src/main/java/com/wine/to/up/winestyle/parser/service/logging/NotableEvents.java
@@ -4,7 +4,7 @@ package com.wine.to.up.winestyle.parser.service.logging;
 import com.wine.to.up.commonlib.logging.NotableEvent;
 
 public enum NotableEvents implements NotableEvent {
-    I_WINE_PAGE_PARSED("Wine page parsed: {}"),
+    I_WINES_PAGE_PARSED("Wine page parsed: {}"),
     I_WINE_DETAILS_PARSED("Wine ({}) parsed details: {}"),
     W_WINE_PAGE_PARSING_FAILED("Failed to parse {} page"),
     W_WINE_DETAILS_PARSING_FAILED("Failed to parse {} page");
